02d797cd88e65c4e7b9cb72518f3e7fd6308e808
Tushar3621766 <mahfujur.tushar@gmail.com>
2022-09-11 12:21:27 +0600

    my

---
+++ loop.js
@@ -0,0 +1,28 @@
+/* if(mark > 70){
+    console.log('gift');
+    if(m > 80){
+        console.log('g');
+    }
+}
+elase{
+    console.log('empty');
+} */
+
+/*  var chocolateGiven = 0;
+
+while(chocolateGiven < 7){
+    console.log('chocolate');
+    chocolateGiven ++;
+}
+ */
+
+
+/* for(var chocolate = 0; chocolate < 7; chocolate++ ){
+    console.log('s');
+} */
+
+var number = [12,14,16,17,18,20,19,10,2]
+for(var i=0; i<number.length; i++ ){
+    var element = number[2];
+    console.log(element);
+}
